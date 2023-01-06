72c01d941aee6e7df67c2d964281346018348075
Punam Rajput <punamrajput777999@gmail.com>
2023-01-06 18:23:07 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
