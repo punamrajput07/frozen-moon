c02a5315d009ce28539952a88cbf226eb3e31c89
Punam Rajput <punamrajput777999@gmail.com>
2023-02-13 12:12:31 +0530

    folder structure

--- server/index.js
+++ server/index.js
@@ -3,11 +3,45 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import User from './models/user';
+
+
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000,()=>{
+mongoose.connect(process.env.MONGODB_URL, () => {
+    console.log('Connect to MongoDB');
+
+})
+
+//api routes starts here
+
+app.post('/signup', (req, res) => {
+    const { name, phone, email, password, role } = req.body;
+
+
+    const user = new User({
+        name: name,
+        phone: phone,
+        email: email,
+        password: password,
+        role: role
+    })
+
+      const savedUser = await user.save();
+
+    res.json({
+        success: true,
+        message: "User created successfully.", 
+        data: savedUser
+    })
+})
+
+
+//api routes ends here 
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
